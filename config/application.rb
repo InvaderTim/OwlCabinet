5abacde45794b63ff02162de4f07835f4deff33e
Tim Robb <tim@robbmob.com>
2011-11-09 10:28:57 +1300

    Renamed 'blog' to VetCalc

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-module Blog
+module VetCalc
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Blog::Application.routes.draw do
+VetCalc::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
