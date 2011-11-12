d065398847756bd44b287ba6e632589bdd649631
Tim Robb <tim@robbmob.com>
2011-11-12 17:09:07 +1300

    Added introduction page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 OwlCabinet::Application.routes.draw do
-  root :to => 'users#index'
-  resources :users
+  root :to => 'home#index'
+
   resources :home
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -49,10 +50,6 @@ OwlCabinet::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
