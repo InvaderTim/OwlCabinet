ae9f2711a7cb8a88ef1a15b08354a475ba91edeb
Tim Robb <tim@robbmob.com>
2011-11-12 16:50:37 +1300

    Added show, edit and add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,59 @@ class UsersController < ApplicationController
     	format.json 	{ render :json => @users }
     end
   end
+
+  def new
+  	@user = User.new
+
+  	respond_to do |format|
+  		format.html 	# new.html.erb
+  		format.json		{ render :json => @post }
+  	end
+  end
+
+  def create
+  	@user = User.new(params[:user])
+
+  	respond_to do |format|
+  		if @user.save
+  			format.html 	{ redirect_to(@user, 
+	  						  :notice => 'Post was succesfully created.') }
+  			format.json		{ render :json => @user,
+  							  :status => :created, :location => @user }
+  		else
+  			format.html 	{ render :action => "new" }
+  			format.json		{ render :json => @user.errors,
+  							  :status => :unprocessable_entity }
+  		end
+  	end
+  end
+
+  def show
+  	@user = User.find(params[:id])
+
+  	respond_to do |format|
+  		format.html 	# show.html.erb
+  		format.json		{ render :json => @user }
+  	end
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+
+  	respond_to do |format|
+  		if @user.update_attributes(params[:user])
+  			format.html 	{ redirect_to(@user,
+	  						  :notice => 'User was successfully updated.') }
+	  		format.json		{ render :json => {}, :status => :ok }
+  		else
+  			format.html 	{ render :action => "edit" }
+  			format.json		{ render :json => @user.errors,
+  							  :status => :unprocessable_entity }
+  		end
+  	end
+  end
 end
